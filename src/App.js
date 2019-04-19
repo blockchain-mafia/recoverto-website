dc89ff1ff356c85d70e9265f6846fa40d35af20f
Nicolas Wagner <contact@wagner-nicolas.com>
2019-04-19 14:19:36 +0300

    feat(header): style header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { slide as Menu } from 'react-burger-menu'
+import TextLoop from 'react-text-loop'
+
 import './App.css'
 
 class App extends Component {
@@ -6,10 +9,39 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          RECOVER WEBSITE
+          <Menu right>
+            <a id="home" className="menu-item" href="/">Home</a>
+            <a id="about" className="menu-item" href="/about">About</a>
+            <a id="contact" className="menu-item" href="/contact">Contact</a>
+          </Menu>
+          <div className="App-header-logo">RECOVER</div>
+          <div className="App-header-wrapper">
+            <div className="App-header-wrapper-content">
+              Lost and Found service
+              <br />
+              <div>
+                for {' '}
+                <TextLoop mask={true} interval={1400}>
+                  <span>Smartphone.</span>
+                  <span>Wallet.</span>
+                  <span>Laptop.</span>
+                  <span>Keys.</span>
+                  <span>Bag.</span>
+                  <span>Tablet.</span>
+                </TextLoop>
+              </div>
+              <div className="App-header-wrapper-content-buttons">
+                <button>Learn More</button>
+                <div className="App-header-wrapper-content-buttons-space">{' '}</div>
+                <button className="btn-solid">Safe your item</button>
+              </div>
+            </div>
+          </div>
         </header>
+        <section>
+        </section>
       </div>
-    );
+    )
   }
 }
 
